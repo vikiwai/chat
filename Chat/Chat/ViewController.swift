956e078bbc70f611a7ad521e0183b6ab5ed5789f
vikiwai <avikiwai@gmail.com>
2020-09-16 21:20:10 +0300

    Initial commit

---
+++ Chat/Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by vikiwai on 16.09.2020.
+//  Copyright © 2020 vikiwai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
