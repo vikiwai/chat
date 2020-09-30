31d1bc8d90968ec8736b96e71917134d3cc9ee2a
Victoria <avikiwai@gmail.com>
2020-09-30 20:32:54 +0300

    Add custom cell class file

---
+++ Chat/Chat/ConversationTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ConversationTableViewCell.swift
+//  Chat
+//
+//  Created by vikiwai on 30.09.2020.
+//  Copyright © 2020 vikiwai. All rights reserved.
+//
+
+import UIKit
+
+class ConversationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+}
