dd4b026af593fb43dfa93a3d44cbf5aa81726f93
Victoria <avikiwai@gmail.com>
2020-09-30 20:41:49 +0300

    Add model for conversation cell

---
+++ Chat/Chat/ConversationCellModel.swift
@@ -0,0 +1,17 @@
+//
+//  ConfigurationCellModel.swift
+//  Chat
+//
+//  Created by vikiwai on 30.09.2020.
+//  Copyright © 2020 vikiwai. All rights reserved.
+//
+
+import Foundation
+
+struct ConversationCellModel {
+    let name: String
+    let message: String
+    let date: String
+    let isOnline: String
+    let hasUnreadMessages: Bool
+}
